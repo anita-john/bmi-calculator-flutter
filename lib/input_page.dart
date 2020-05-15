4aeead242f1edbb46e57c0c8384e3bac9881dc35
Anita John <anitaj1@learn.cssd.ab.ca>
2020-05-15 16:09:37 -0600

    added the button slider and button

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,11 +1,9 @@
 import'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'constants.dart';
 
 
-const bottomContainerHeight = 80.0;
-const activeCardColour = Color(0xFF1D1E33);
-const inactiveCardColour = Color(0xFF111328);
-const bottomCardColour = Color(0xFFEB1555);
+
 enum Gender {
   male,
   female,
@@ -17,6 +15,7 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   Gender selectedGender;
+  int height =180;
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +24,7 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch ,
         children: <Widget>[
           Expanded(child: Row (
             children: <Widget>[
@@ -34,7 +34,7 @@ class _InputPageState extends State<InputPage> {
                     selectedGender = Gender.male;
                   });
                 },
-                colour:selectedGender == Gender.male ? activeCardColour: inactiveCardColour,
+                colour:selectedGender == Gender.male ? kActiveCardColour: kInactiveCardColour,
                 cardChild: IconContent(
                   icon: FontAwesomeIcons.mars,
                   label: 'MALE',
@@ -47,7 +47,7 @@ class _InputPageState extends State<InputPage> {
                     selectedGender = Gender.female;
                   });
                 },
-                colour: selectedGender == Gender.female ? activeCardColour: inactiveCardColour,
+                colour: selectedGender == Gender.female ? kActiveCardColour: kInactiveCardColour,
                 cardChild: IconContent(
                   icon: FontAwesomeIcons.venus,
                   label:'FEMALE',
@@ -57,25 +57,58 @@ class _InputPageState extends State<InputPage> {
             ],
           ),
           ),
-          Expanded(child: ReusableCard(colour: activeCardColour
+          Expanded(child: ReusableCard(colour: kActiveCardColour,
+            cardChild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[Text('HEIGHT', style: kLabelTextStyle ,
+              ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: <Widget>[
+                    Text(
+                      height.toString(),
+                      style: kNumberTextStyle,
+                    ),
+                    Text(
+                      'cm',
+                      style: kLabelTextStyle,
+                    ),
+                  ],
+                ),
+                Slider(
+                  value: height.toDouble(),
+                  min: 120.0,
+                  max: 220.0,
+                  activeColor: Color(0xFFEB1555),
+                  inactiveColor: Color(0xFF8D8E98),
+                  onChanged: (double newValue){
+                    setState(() {
+                      height = newValue.round();
+                    });
+                  } ,
+                ),
+              ],
+            ),
           ),
           ),
           Expanded(child: Row(
         children: <Widget>[
-          Expanded(child: ReusableCard(colour: activeCardColour
+          Expanded(child: ReusableCard(colour: kActiveCardColour
           ),
           ),
-          Expanded( child: ReusableCard(colour: activeCardColour
+          Expanded( child: ReusableCard(colour: kActiveCardColour
           ),
           ),
     ],
     ),
           ),
           Container(
-            color: bottomCardColour,
+            color: kBottomCardColour,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: bottomContainerHeight,
+            height: kBottomContainerHeight,
           ),
         ],
       ),
@@ -84,10 +117,7 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-const labelTextStyle = TextStyle(
-  fontSize: 18.0,
-  color: Color( 0xFF8D8E98),
-);
+
 class IconContent extends StatelessWidget {
   IconContent({this.icon, this.label});
   final IconData icon;
@@ -107,7 +137,7 @@ class IconContent extends StatelessWidget {
           height: 15.0,
         ),
         Text(label,
-          style: labelTextStyle,
+          style: kLabelTextStyle,
         ),
       ],
     );
